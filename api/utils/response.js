090e9a3e59bba71e3ff319c495c17c11e63abe8f
Yiomar <yiomar.rc@gmail.com>
2021-09-05 11:37:06 -0500

    Initial

---
+++ api/utils/response.js
@@ -0,0 +1,27 @@
+const success = (res, successRes = {}, status = 200) => {
+  res.status(status).send({
+    success: true,
+    body: {
+      successCode: status,
+      successMessage: successRes.successMessage || 'Ok',
+      data: successRes.data || {}
+    },
+  });
+};
+
+const error = (res, errorRes = {}, status = 400) => {
+  res.status(status).send({
+    success: false,
+    error: {
+      errorCode: status,
+      errorMessage: errorRes.errorMessage || 'An error occurred',
+      data: errorRes.data || {},
+      errorMessageDetail: errorRes.errorMessageDetail || 'Error detail',
+    },
+  });
+};
+
+module.exports = {
+  success,
+  error,
+};
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = new express.Router();
+const handleResponse = require('./api/utils/response');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(router);
+
+router.get('/message', (_, res) => {
+  handleResponse.success(res);
+});
+
+router.post('/message', (_, res) => {
+  handleResponse.error(res);
+});
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log((`-> API is running in https://localhost:${port}`));
+});
