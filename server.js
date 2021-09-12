60ba45031d4a45553c91fd34e6d4f4d5edd9a8bf
Yiomar <yiomar.rc@gmail.com>
2021-09-12 16:18:31 -0500

    New Structure

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 require('dotenv').config();
 const router = require('./api/network/routes')
 
+
+
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
